a059575477244195a931752b7ec8651fe65cf0f9
Ryo Suzuki <astronautryosuzuki@gmail.com>
2022-06-15 00:30:52 +0900

    a

--- input_data.py
+++ input_data.py
@@ -6,8 +6,10 @@ class Read:
         pass
 
     def read_json(path):
-        path = path
         return json.load(open(path,"r"))
+    
+    def read_df(path):
+        return pd.read_csv(path)
 
 class Write:
     def __init__(self):
@@ -36,11 +38,18 @@ class Write:
                 ll.append(l)
         print(ll)
         return pd.DataFrame(ll,columns=cols)
+    
+    def write_to_csv(df:pd.DataFrame,file_path:str):
+        df.to_csv(file_path,index=False)
 
 def main():
     read = Read
+    df = read.read_df("Battle_Report.csv")
     cols = read.read_json("cols.json")
-    Write.add_db(cols)
+    add_df = Write.add_db(cols)
+    df.append(add_df)
+    Write.write_to_csv(df,"Battle_Report.csv")
+
 
 
 if __name__ == "__main__":
